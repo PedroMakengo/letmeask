d9a55c6fe5ebac2376a452b6dba3b44ce644e690
PedroMakengo <61626420+PedroMakengo@users.noreply.github.com>
2021-08-03 14:07:35 -0300

    Trabalhando com as alterações do sistema

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,17 +1,21 @@
+import { FormEvent, useState } from 'react';
 import { useHistory } from 'react-router';
 
 import ilLustrationImg from '../assets/images/illustration.svg';
 import logoImg from '../assets/images/logo.svg';
 import googleIconImg from '../assets/images/google-icon.svg';
 
+import { database } from '../services/firebase';
+
 import { Button } from '../components/Button';
+import { useAuth } from '../hooks/useAuth';
 
 import '../styles/auth.scss';
-import { useAuth } from '../hooks/useAuth';
 
 export function Home() {
   const history = useHistory();
   const { user, signInWithGoogle } = useAuth();
+  const [roomCode, setRoomCode] = useState('');
 
   async function handleCreateRoom() {
     if (!user) {
@@ -20,8 +24,21 @@ export function Home() {
     history.push('/rooms/new');
   }
 
-  async function handleJoinRoom() {
+  async function handleJoinRoom(event: FormEvent) {
+    event.preventDefault();
 
+    if(roomCode.trim() === '') {
+      return;
+    }
+
+    const roomRef = await database.ref(`rooms/${roomCode}`).get();
+
+    if(!roomRef.exists()) {
+      alert('Room does not exists.');
+      return;
+    }
+
+    history.push(`/rooms/${roomCode}`);
   }
 
   return (
@@ -43,6 +60,8 @@ export function Home() {
             <input
               type="text"
               placeholder="Digite o código da sala"
+              onChange={event => setRoomCode(event.target.value)}
+              value={roomCode}
             />
             <Button type="submit">
               Entrar na sala
--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -1,5 +1,53 @@
+import { useState } from 'react';
+import { useParams } from 'react-router-dom';
+
+import logoImg from '../assets/images/logo.svg';
+import { Button } from '../components/Button';
+import { RoomCode } from '../components/RoomCode';
+
+import '../styles/room.scss';
+
+type RoomParams = {
+  id: string;
+}
+
 export function Room() {
+  const params = useParams<RoomParams>();
+  const [newQuestion, setNewQuestion] = useState('');
+  const roomId = params.id;
+
+  async function handleSendQuestion() {
+
+  }
+
   return (
-    <h1>Sala</h1>
+    <div id="page-room">
+      <header>
+        <div className="content">
+          <img src={logoImg} alt="Letmeask" />
+          <RoomCode code={roomId} />
+        </div>
+      </header>
+
+      <main>
+        <div className="room-title">
+          <h1>Sala React</h1>
+          <span>4 perguntas</span>
+        </div>
+
+        <form>
+          <textarea 
+            placeholder="O que você quer perguntar?"
+            onChange={event => setNewQuestion(event.target.value)}
+            />
+          <div className="form-footer">
+            <span>Para enviar uma pergunta, <button>faça seu login</button></span>
+            <Button type="submit"> 
+              Enviar pergunta
+            </Button>
+          </div>
+        </form>
+      </main>
+    </div>
   );
 }
