b0a50c0c8be222cdd446a5f0b1c09461d0ff0e13
PedroMakengo <61626420+PedroMakengo@users.noreply.github.com>
2021-08-04 06:55:28 -0300

    Trabalhando no projeto

--- src/components/RoomCode.tsx
+++ src/components/RoomCode.tsx
@@ -7,7 +7,7 @@ type RoomCodeProps = {
 
 export function RoomCode(props: RoomCodeProps) {
   function copyRoomCodeToClipboard() {
-    navigator.clipboard.writeText(props.code)
+    navigator.clipboard.writeText(props.code);
   }
 
   return(
--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -1,23 +1,94 @@
-import { useState } from 'react';
+import { useEffect } from 'react';
+import { FormEvent, useState } from 'react';
 import { useParams } from 'react-router-dom';
 
 import logoImg from '../assets/images/logo.svg';
 import { Button } from '../components/Button';
 import { RoomCode } from '../components/RoomCode';
+import { useAuth } from '../hooks/useAuth';
+import { database } from '../services/firebase';
 
 import '../styles/room.scss';
+
+type FirebaseQuestions = Record<string, {
+  author: {
+    name: string;
+    avatar: string;
+  }
+  content: string;
+  isAnswered: boolean;
+  isHighlighted: boolean;
+}>;
+
+type Question = {
+  id: string;
+  author: {
+    name: string;
+    avatar: string;
+  }
+  content: string;
+  isAnswered: boolean;
+  isHighlighted: boolean;
+}
 
 type RoomParams = {
   id: string;
 }
 
 export function Room() {
+  const { user } = useAuth();
   const params = useParams<RoomParams>();
   const [newQuestion, setNewQuestion] = useState('');
+  const [questions, setQuestions] = useState<Question[]>([]);
+  const [title, setTitle] = useState('');
+
   const roomId = params.id;
 
-  async function handleSendQuestion() {
+  useEffect(() => {
+    const roomRef = database.ref(`rooms/${roomId}`);
 
+    roomRef.on('value', room => {
+      const databaseRoom = room.val();
+      const firebaseQuestions: FirebaseQuestions = databaseRoom.questions ?? {};
+
+      const parsedQuestions = Object.entries(firebaseQuestions).map(([key, value]) => {
+        return {
+          id: key, 
+          content: value.content,
+          author: value.author, 
+          isHighlighted: value.isHighlighted,
+          isAnswered: value.isAnswered,
+        }
+      });
+      setTitle(databaseRoom.title);
+      setQuestions(parsedQuestions);
+    });
+  }, [roomId])
+
+  async function handleSendQuestion(event: FormEvent) {
+    event.preventDefault();
+    if(newQuestion.trim() === '') {
+      return;
+    }
+
+    if(!user) {
+      throw new Error('You must be logged in');
+    }
+
+    // Salvando as questões
+    const question = {
+      content: newQuestion,
+      author: {
+        name: user.name, 
+        avatar: user.avatar
+      },
+      isHighlighted: false,
+      isAnswered: false
+    }
+
+    await database.ref(`/rooms/${roomId}/questions`).push(question);
+
+    setNewQuestion('');
   }
 
   return (
@@ -31,18 +102,28 @@ export function Room() {
 
       <main>
         <div className="room-title">
-          <h1>Sala React</h1>
-          <span>4 perguntas</span>
+          <h1>{ title }</h1>
+          { questions.length > 0 && <span>{ questions.length } pergunta(s)</span> }
         </div>
 
-        <form>
+        <form onSubmit={handleSendQuestion}>
           <textarea 
             placeholder="O que você quer perguntar?"
             onChange={event => setNewQuestion(event.target.value)}
+            value={newQuestion}
             />
           <div className="form-footer">
-            <span>Para enviar uma pergunta, <button>faça seu login</button></span>
-            <Button type="submit"> 
+            {user ? (
+              <div className="user-info">
+                <img src={user.avatar} alt={user.name} />
+                <span>{user.name}</span>
+              </div>
+            ) : ( 
+              <span>Para enviar uma pergunta, 
+                <button>faça seu login</button>
+              </span>
+            ) }
+            <Button type="submit" disabled={!user}> 
               Enviar pergunta
             </Button>
           </div>
